db54ba0817d7e9a10b6f4aaeacd0fca347217cd1
Sir Louis <me@sirlouis.io>
2020-04-19 23:29:14 +0100

    bug fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $page_name = "policy";
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
-    <?php \setup_page('policy','base', true, PRJ_HEADER_HEIGHT); ?>
+    <?php \TymFrontiers\Helper\setup_page('policy','base', true, PRJ_HEADER_HEIGHT); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
       <div class="view-space">
